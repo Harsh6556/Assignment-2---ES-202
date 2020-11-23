3bb2c5bf00648cb95d653f55a7a7099d22973a32
Harsh6556 <54997627+Harsh6556@users.noreply.github.com>
2020-11-23 11:35:03 +0530

    Delete Q1.c

--- Q1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() 
-{
-    char ch; // declare a variable of datatyoe char
-    for (ch = 'a'; ch<= 'z'; ++ch) // for loop to display alphabets from 'a' to 'z'
-    printf("%c ", ch); // display the alphabets
-    return 0;
-}
