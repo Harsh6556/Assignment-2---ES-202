b188bd614a40bbd07bde80e47b082b06f0efc9cc
Harsh6556 <54997627+Harsh6556@users.noreply.github.com>
2020-11-24 10:33:56 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // header file
+int main()
+{
+	int i; // declare some variables
+	printf("Even numbers between 1 to 100 (inclusive) are \n");
+	for (i = 1; i <= 100; i++) // for loop
+	{
+		if(i%2 == 0) // check for even numbers
+		{
+		  printf("%d ", i); // print even numbers
+	}
+}
+return 0;
+}
