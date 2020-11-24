11e15d89caf291fa17a012cb8bbc60959f996cee
Harsh6556 <54997627+Harsh6556@users.noreply.github.com>
2020-11-24 10:33:13 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() 
+{
+    char ch; // declare a variable of datatype char
+    for (ch = 'a'; ch<= 'z'; ++ch) // for loop to display alphabets from 'a' to 'z'
+    printf("%c ", ch); // display the alphabets
+    return 0;
+}
