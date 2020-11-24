6cb1bb500ab6d7323c091fbf19da5925f677d768
Harsh6556 <54997627+Harsh6556@users.noreply.github.com>
2020-11-24 10:34:50 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // header files
+int main()
+{
+    int n, c = 0; // declare some variables
+    
+    printf("Enter any integer- \n "); // ask for user input
+    scanf("%d", &n); // read in user input
+ 
+    while (n != 0) // iterate until n becomes 0
+    {
+        n /= 10; // remove last digit from n   
+        c++;     // increase counter value by 1
+    }
+
+    printf("Number of digits: %d", c); // display the result
+}
